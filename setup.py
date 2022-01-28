563ba00c0f50d06fadcb6e7dd3952edd4f0a015a
Yuriy Cherniy <amidobox@gmail.com>
2022-01-28 23:31:38 +0300

    test

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='django-wysiwyg-image',
-    version='0.1',
+    version='0.1.1',
     packages=['wysiwyg_img', 'wysiwyg_img.migrations'],
     requires=['python (>=3.6)', 'django (>=3.2)'],
     description='An easy way to paste images to wysiwyg editors in Django admin interface.',
+    long_description=open('./README.rst', 'r').read(),
     author='Yuriy Cherniy',
     author_email='amidobox@gmail.com',
     url='https://github.com/YuriyCherniy/django-wysiwyg-image',
