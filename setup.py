ca8c42f187a642737a8a7b40e104a53b77fa63b2
Yuriy Cherniy <amidobox@gmail.com>
2022-01-28 23:43:56 +0300

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-wysiwyg-image',
-    version='0.1.1',
+    version='0.1.2',
     packages=['wysiwyg_img', 'wysiwyg_img.migrations'],
     requires=['python (>=3.6)', 'django (>=3.2)'],
     description='An easy way to paste images to wysiwyg editors in Django admin interface.',
