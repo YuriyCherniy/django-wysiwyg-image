22222316014b8d8042e729d842886635cee61c48
Yuriy Cherniy <amidobox@gmail.com>
2022-01-28 23:35:07 +0300

    long_description_content_type added

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     requires=['python (>=3.6)', 'django (>=3.2)'],
     description='An easy way to paste images to wysiwyg editors in Django admin interface.',
     long_description=open('./README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     author='Yuriy Cherniy',
     author_email='amidobox@gmail.com',
     url='https://github.com/YuriyCherniy/django-wysiwyg-image',
